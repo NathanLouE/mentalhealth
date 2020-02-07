73c3218d0171683f087428ce60dc2112bdc4fb97
Glitch (thrilling-cormorant) <none>
2020-02-07 23:12:06 +0000

    🚈🎻 Updated with Glitch

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+$( document ).ready(function() {
+    // randomPage();
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+$( function() {
+    $( document ).tooltip();
+});
+
+$(".brain").click(function(){
+  randomPage();
+});
+
+function randomPage () {
+  let pageVar = Math.random();
+  if (pageVar < 0.33) {
+    document.getElementById("brainLink").href = "/Anxiety.html";
+  } else if (pageVar < 0.67) {
+    document.getElementById("brainLink").href = "/Schizophrenia.html";
+  } else {
+    document.getElementById("brainLink").href = "/Depression.html";
+  }
+  console.log (pageVar);
+}
+});
+$("li").hover(function(){
+  $(this).css("opacity",100)
+// console.log(this)
+});
+
